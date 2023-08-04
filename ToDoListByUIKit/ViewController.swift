952625e3df059ab43adda6bc0fe64781095be82b
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2023-08-04 21:55:26 +0900

    tabaleViewの準備

--- ToDoListByUIKit/ViewController.swift
+++ ToDoListByUIKit/ViewController.swift
@@ -8,6 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private weak var tableView: UITableView!
+    
+    override func loadView() {
+        super.loadView()
+        let view = UIView()
+        let tableView = UITableView()
+        tableView.backgroundColor = .systemBlue
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+        
+        self.view = view
+        self.tableView = tableView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
